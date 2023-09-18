c11d2a52be4cacf61758bbb1110ff82dcf69ace0
ch3ayba <zaidaneb@gmail.com>
2023-09-18 01:15:58 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -2,7 +2,7 @@
 #include <stdarg.h>
 /**
  * flage - Description of the function.
- * @format : type of the varaible
+ * @forma : type of the varaible
  * @args : argument list
  * @lent : kep traque of the argument passe
  * @...: Description of the variadic arguments.
@@ -21,38 +21,41 @@ void flage(va_list args, const char forma, int *lent)
 	if (forma == 'b')
 		_putb2(va_arg(args, unsigned int), lent);
 	if (forma == 'n')
-		_putchar('\n',lent);
+		_putchar('\n', lent);
 	if (forma == 'X' || forma == 'x')
 		_putXx(va_arg(args, unsigned int), forma, lent);
 	if (forma == 'u')
 		_putnbr_u(va_arg(args, int), lent);
-	/*if (forma == 'o')
-		_putb8(va_arg(args, unsigned int ), lent);*/
+/*
+ * if (forma == 'o')
+ * _putb8(va_arg(args, unsigned int ), lent);
+ */
 }
 
 /**
  * _printf - Description of the function.
- * @format : type of the varaible
+ * @forma : type of the varaible
  * @...: Description of the variadic arguments.
  *
  * Return: Description of the return value (if applicable).
  */
-int _printf(const char *format, ...)
+int _printf(const char *forma, ...)
 {
 	int lent, i;
 	va_list args;
+
 	lent = 0;
 	i = 0;
-	va_start(args, format);
-	while (format[i] && *format)
+	va_start(args, forma);
+	while (forma[i] && *forma)
 	{
-		if (format[i] == '%')
+		if (forma[i] == '%')
 		{
 			i++;
-			flage(args, format[i], &lent);
+			flage(args, forma[i], &lent);
 		}
 		else
-			_putchar(format[i], &lent);
+			_putchar(forma[i], &lent);
 		i++;
 	}
 		va_end(args);
