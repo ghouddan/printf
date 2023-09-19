cce231780cdd25e70fc9ac4a861977cbf3dce1d6
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-19 14:43:10 -0700

    fixe betty-style and doc

--- _put_add.c
+++ _put_add.c
@@ -1,43 +1,43 @@
 #include "main.h"
 
 /**
- * _putXx - Description of the function.
+ * _put16- Description of the function.
  * @n : number to convert;
  * @x : FF or ff
- * @lent: Description of the variadic arguments.
+ * @lent: counter
  *
  * Return: Description of the return value (if applicable).
  */
 void _put16(unsigned long n, char x, int *lent)
 {
-        unsigned int baseX;
-        char *base;
+	unsigned int baseX;
+	char *base;
 
-        baseX = 16;
-        if (x == 'x')
-                base = "0123456789abcdef";
-        else
-                base = "0123456789ABCDEF";
-        if (n < baseX)
-                _putchar(base[n % baseX], lent);
-        else
-        {
-                _put16(n / baseX, x, lent);
-                _put16(n % baseX, x, lent);
-        }
+	baseX = 16;
+	if (x == 'x')
+		base = "0123456789abcdef";
+	else
+		base = "0123456789ABCDEF";
+	if (n < baseX)
+		_putchar(base[n % baseX], lent);
+	else
+	{
+		_put16(n / baseX, x, lent);
+		_put16(n % baseX, x, lent);
+	}
 }
 /**
  * _put_add - writes the character c to stdout
- * @p: pointer to address
+ * @add: pointer to address
  * @lent : counter
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-void _put_add(void *add, int *len)
+void _put_add(void *add, int *lent)
 {
 	unsigned long int pointer;
 
 	pointer = (unsigned long int)add;
-	_putstr("0x", len);
-	_put16(pointer, 'x', len);
+	_putstr("0x", lent);
+	_put16(pointer, 'x', lent);
 }
