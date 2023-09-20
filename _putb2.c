b51f2036631af82d10b315c54a140a510ef3a62e
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-20 14:44:27 -0700

    fixe error

--- _putb2.c
+++ _putb2.c
@@ -7,18 +7,23 @@
  */
 void _putb2(int nb, int *lent)
 {
-	int i = 0, j;
+	 int i = 0, j;
+	unsigned int n;
+	if (nb < 0)
+		n = (unsigned int)-nb;
+	else
+		n = (unsigned int)nb;
 
-	if (nb == 0)
+	if (n == 0)
 	{
 		_putchar('0', lent);
 		return;
 	}
-	while ((1 << i) <= nb)
+	while ((unsigned int)(1 << i) <= n)
 		i++;
 	for (j = i - 1; j >= 0; j--)
 	{
-		if (nb & (1 << j))
+		if (n & (1 << j))
 			_putchar('1', lent);
 		else
 			_putchar('0', lent);
--- _putchar.c
+++ _putchar.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <unistd.h>
-
+#define SIZE 1024
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -11,6 +11,17 @@
  */
 void _putchar(char c, int *lent)
 {
+	static char buffer[SIZE];
+	static int index;
+	
+	buffer[++index] = c;
+	
+	if (index == SIZE)
+	{
+		write(1, buffer, SIZE);
+		(*lent) += SIZE;
+		index = 0;
+	}
 	write(1, &c, 1);
 	(*lent)++;
 }
